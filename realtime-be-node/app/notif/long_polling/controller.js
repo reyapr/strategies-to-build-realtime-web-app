ed123b8a16f3ac7148a06c09c8f18f7f627e7c45
Renal Apriansyah <renal.apr@gmail.com>
2023-03-06 14:23:52 +0700

    add script run the server

--- realtime-be-node/app/notif/long_polling/controller.js
+++ realtime-be-node/app/notif/long_polling/controller.js
@@ -4,18 +4,20 @@ const sleep = require('../../helper/sleep');
 
 const getNotif = (notifCollection) => async (req, res) => {
   try {
-    const requestedDate = req.query.requestedDate
+    const requestDate = req.query.requestDate
     
     const intrvl = setInterval(async () => {
       const response = await notifCollection.findOne({ _id: new ObjectId('64032116e05e21c3ae61e530')})
-      console.log( `<=================== masuk ==================`);
-      if (new Date(response.updatedAt) >= new Date(requestedDate)) {
+      if (new Date(response.updatedAt) >= new Date(requestDate)) {
         res.status(200).json(response)
       } 
     }, 1000);
     
     const timeout = setTimeout(() => {
       clearInterval(intrvl)
+      res.status(408).json({
+        timeout: true
+      })
     }, 10000);
     
     req.on('close', () => {
